a6e507080a0101ce24e27796493e7af7a120afcb
= <filipe.sf04@gmail.com>
2023-08-26 07:25:14 -0300

    kinda made the assignment

--- src/Board.lua
+++ src/Board.lua
@@ -13,9 +13,10 @@
 
 Board = Class{}
 
-function Board:init(x, y)
+function Board:init(x, y, level)
     self.x = x
     self.y = y
+    self.level = level
     self.matches = {}
 
     self:initializeTiles()
@@ -32,7 +33,11 @@ function Board:initializeTiles()
         for tileX = 1, 8 do
             
             -- create a new tile at X,Y with a random color and variety
-            table.insert(self.tiles[tileY], Tile(tileX, tileY, math.random(18), math.random(6)))
+            if self.level == 1 then
+                table.insert(self.tiles[tileY], Tile(tileX, tileY, math.random(10), 1))
+            else
+                table.insert(self.tiles[tileY], Tile(tileX, tileY, math.random(10), math.random(6)))
+            end
         end
     end
 
@@ -240,7 +245,11 @@ function Board:getFallingTiles()
             if not tile then
 
                 -- new tile with random color and variety
-                local tile = Tile(x, y, math.random(18), math.random(6))
+                local variety = math.random(6)
+                if self.level == 1 then
+                    variety = 1
+                end
+                local tile = Tile(x, y, math.random(18), variety)
                 tile.y = -32
                 self.tiles[y][x] = tile
 
--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -61,7 +61,7 @@ function PlayState:enter(params)
     self.level = params.level
 
     -- spawn a board and place it toward the right
-    self.board = params.board or Board(VIRTUAL_WIDTH - 272, 16)
+    self.board = params.board or Board(VIRTUAL_WIDTH - 272, 16, params.level)
 
     -- grab score from params if it was passed
     self.score = params.score or 0
@@ -231,7 +231,10 @@ function PlayState:calculateMatches()
 
         -- add score for each match
         for k, match in pairs(matches) do
-            self.score = self.score + #match * 50
+            for l, tile in pairs(match) do
+                self.score = self.score + (tile.variety * 200)
+            end
+            self.timer = self.timer + 1
         end
 
         -- remove any tiles that matched from the board, making empty spaces
